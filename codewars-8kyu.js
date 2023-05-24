3f72c7d0867200a6bda0ce0887fad5471317fcc0
Andrei Seandru <dunkradu1234@gmail.com>
2023-05-24 16:32:37 +0300

    century

--- codewars-8kyu.js
+++ codewars-8kyu.js
@@ -116,3 +116,10 @@ function basicOp(operation, value1, value2) {
     }
 }
 
+function century(year) {
+    let century = Math.floor(year/100);
+    if(year % 100 !== 0){
+      century += 1;
+    }
+    return century;
+}
