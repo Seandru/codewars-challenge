27db7adca160a82bf0914beab2df0ce7aac1cf8e
Andrei Seandru <dunkradu1234@gmail.com>
2023-05-24 13:36:27 +0300

    positiveSum

--- codewars-8kyu.js
+++ codewars-8kyu.js
@@ -2,5 +2,13 @@ function multiply(a, b){
     return a * b;
 }
 
-
+function positiveSum(arr) {
+    let sum = 0;
+    for(i = 0 ; i < arr.length ; i++){
+      if(arr[i] > 0){
+        sum += arr[i];
+      }
+    }
+    return sum;
+}
   
