4359061964abe8fb8e3f88d0335341d98c24351f
Andrei Seandru <dunkradu1234@gmail.com>
2023-05-25 16:52:51 +0300

    accum

--- codewars-8kyu.js
+++ codewars-8kyu.js
@@ -53,6 +53,16 @@ function getMiddle(s){
   return middleChar;
 } 
 
+function accum(s) {
+  let result = '';
+  for (let i = 0; i < s.length; i++) {
+    result += s[i].toUpperCase() + s[i].toLowerCase().repeat(i);
+    if (i !== s.length - 1) {
+      result += '-';
+    }
+  } 
+  return result;
+}
 
 // 8kyu
 
