f9215312cd8bf2bee5253af684a2c63b65766ed4
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-06 14:41:52 +0300

    add func that returns sum of nth row numbers

--- codewars-7kyu.js
+++ codewars-7kyu.js
@@ -250,3 +250,14 @@ function isTriangle(a, b, c) {
   }
   return false;
 }
+
+// The rowSumOddNumbers function returns the sum of the nth row of this triangle
+//   1
+// 3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+function rowSumOddNumbers(n) {
+	return n*n*n
+}
+
