c46e1aba4b8f81834e1a97221f383116b2780196
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-06 14:54:20 +0300

    add func that checks the sum is odd or even

--- codewars-7kyu.js
+++ codewars-7kyu.js
@@ -273,3 +273,12 @@ function solution(str, ending){
   }
   return true;
 }
+
+// The oddOrEven function checks wheter the sum of the given array's elements is odd or even
+function oddOrEven(array) {
+  let sum = 0;
+ for (let i = 0; i < array.length; i++) {
+   sum += array[i];
+ }
+ return sum % 2 === 0 ? "even" : "odd";
+}
