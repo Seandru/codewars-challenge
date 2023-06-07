90c0cd1bee5d524ca6408ffd09970f940d5814b6
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-07 16:34:11 +0300

    add func that returns the sum of multiples of 3&5

---
+++ codewars-6kyu.js
@@ -0,0 +1,14 @@
+// The solution function returns the sum of all multiples of 3 and 5 below a given number
+function solution(number){
+    if (number < 0) {
+      return 0;
+    }
+    let sum = 0;
+    for (let i = 3; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+  
