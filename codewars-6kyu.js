4f0d3a9a19c02d16f176caf510527ac5b012a27b
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-08 16:45:36 +0300

    add func that return sum of all digits in a number

--- codewars-6kyu.js
+++ codewars-6kyu.js
@@ -10,12 +10,24 @@ function solution(number){
       }
     }
     return sum;
-  }
+}
 
 // The spinWords function returns the given string with every word that has a length greater
 // or equal to 5 characters, reversed.
 function spinWords(string) {
     return string.split(' ').map(word => word.length >= 5 ? word.split('').reverse().join('') : word ).join(' ');
- }
- 
+}
+
+// The digitalRoot function returns the sum of all the digits in a number. 
+function digitalRoot(n) {
+  while (n >= 10) {
+    let sum = 0;
+    while (n > 0) {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    }
+    n = sum;
+  }
+  return n;
+}
   
