bf2b11cf50813f6c01ac769d0a466262116c35ba
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-08 16:56:53 +0300

    add func that generates a string based on arr elem

--- codewars-6kyu.js
+++ codewars-6kyu.js
@@ -30,4 +30,20 @@ function digitalRoot(n) {
   }
   return n;
 }
+
+// The function checks the length of the given array and 
+// generates a string output based on the number of elements in the array.
+function likes(names) {
+  if(names.length === 0){
+    return "no one likes this";
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length > 3) {
+    return `${names[0]}, ${names[1]} and ${(names.length - 2)} others like this`;
+  }
+}
   
