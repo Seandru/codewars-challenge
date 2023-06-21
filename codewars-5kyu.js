6ae69032d332003087c8dcc571b3e6d6364f366c
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-21 18:38:11 +0300

    add fn that returns the score of 5 dice throw

--- codewars-5kyu.js
+++ codewars-5kyu.js
@@ -156,3 +156,58 @@ function incrementString (strng) {
     return strng.replace(/\d+$/, padString);
   }
 }
+
+// The score function calculates the score for a dice throw counting the occurrences
+// of each number, applying the scoring rules and returning the score.
+function score( dice ) {
+  let six = 0;
+  let five = 0;
+  let four = 0; 
+  let three = 0; 
+  let too = 0;
+  let one = 0;
+  let i = 0;
+  while (i < 5) {
+    if (dice[i] == 6) {
+      six++;
+    }
+    if (dice[i] == 5) {
+      five++; 
+    }
+    if (dice[i] == 4) {
+      four++; 
+    }
+    if (dice[i] == 3) {
+      three++; 
+    }
+    if (dice[i] == 2) {
+      too++; 
+    }
+    if (dice[i] == 1) {
+      one++; 
+    }
+    i++;
+  }
+  let r = 0;
+  if (one > 2) {
+    r += 1000; one -= 3;
+  }
+  if (six > 2) {
+    r += 600;
+  }
+  if (five > 2) {
+    r += 500; five -= 3;
+  }
+  if (four > 2) {
+    r += 400; 
+  }
+  if (three > 2) {
+    r += 300; 
+  }
+  if (too > 2) {
+    r += 200; 
+  }
+  r += one * 100;
+  r += five * 50;
+  return r;
+}
